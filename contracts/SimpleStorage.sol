e8856c7c9a6c323bdd5c81d61917412f3476ba99
abduhaseebmir <mabdulhaseebq@gmail.com>
2022-07-11 22:47:34 +0500

    Removed extra comments

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,10 +1,7 @@
-// I'm a comment!
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
   uint256 favoriteNumber;
@@ -14,7 +11,6 @@ contract SimpleStorage {
     string name;
   }
 
-  // uint256[] public anArray;
   People[] public people;
 
   mapping(string => uint256) public nameToFavoriteNumber;
---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const { ethers } = require("hardhat")
+
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Contract deployed at ${simpleStorage.address}`)
+}
+
+main().then(()=> process.exit(0)).catch((error)=> {
+    console.error(error);
+    process.exit(1);
+})
